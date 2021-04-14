0b27200aae5790af2899f89d3aa71cd45169e4d7
oloomoses <olooine@gmail.com>
2021-04-14 23:19:32 +0300

    create Navbar component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Companies from './components/companies/Companies';
 import Search from './components/companies/Search';
+import Navbar from './components/layout/Navbar';
 // import Company from './components/companies/Company';
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
 
   useEffect(() => {
     (async () => {
-      const url = 'https://financialmodelingprep.com/api/v3/stock/list?apikey=3830446fe7a4c1873641ce52d4681a53';
+      const url = 'https://financialmodelingprep.com/api/v3/actives?apikey=3830446fe7a4c1873641ce52d4681a53';
       setLoading(true);
       const res = await axios.get(url);
       setCompanies(res.data);
@@ -22,11 +23,10 @@ function App() {
     })();
   }, []);
 
-  const searchCompany = async (ticker) => {
-    const url = `https://financialmodelingprep.com/api/v3/search?query=AA&limit=10&exchange=${ticker}&apikey=3830446fe7a4c1873641ce52d4681a53`;
+  const searchCompany = async (stock) => {
+    const url = `https://financialmodelingprep.com/api/v3/${stock}?apikey=3830446fe7a4c1873641ce52d4681a53`;
     setLoading(true);
     const res = await axios.get(url);
-    console.log(res.data);
     setCompanies(res.data);
     setLoading(false);
   };
@@ -41,10 +41,13 @@ function App() {
   // };
 
   return (
-    <div className="container">
-      <Search searchCompany={searchCompany} />
-      <Companies loading={loading} companies={companies} />
-      {/* <Company /> */}
+    <div>
+      <Navbar />
+      <div className="container">
+        <Search searchCompany={searchCompany} />
+        <Companies loading={loading} companies={companies} />
+        {/* <Company /> */}
+      </div>
     </div>
   );
 }
