86f192f719b08f8eff9ffd6eee96c57b48d11fcf
oloomoses <olooine@gmail.com>
2021-04-23 00:29:48 +0300

    Add styling for the Company component

--- src/components/companies/Company.js
+++ src/components/companies/Company.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { Card } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { companyData } from '../../redux/stock/stockAction';
@@ -28,14 +29,44 @@ const Company = () => {
 
   return (
     <div className="container">
-      <div>{symbol}</div>
-      <div>{price}</div>
-      <div>{changes}</div>
-      <div>{companyName}</div>
-      <div>{website}</div>
-      <div>{description}</div>
-      <div>{industry}</div>
-      <div>{image}</div>
+      <div className="d-flex align-items-center justify-content-between my-3">
+        <h2 className="">
+          <span className="mr-1">{companyName}</span>
+          (
+          {symbol}
+          )
+        </h2>
+        <h3>
+          <span className="badge badge-info mr-3">Price:</span>
+          {price}
+        </h3>
+      </div>
+      <Card>
+        <Card.Body className="border-bottom">
+          <div className="d-flex align-items-center">
+            <img src={image} alt="company-img" className="rounded-circle mr-4" />
+            <div>{description}</div>
+          </div>
+        </Card.Body>
+
+        <div className="mx-auto my-2">
+          <h4>
+            <span className="badge badge-info mr-3">Change in Price :</span>
+            {changes}
+          </h4>
+
+          <h4>
+            <span className="badge badge-info mr-3">Website :</span>
+            {website}
+          </h4>
+
+          <h4>
+            <span className="badge badge-info mr-3">Industry :</span>
+            {industry}
+          </h4>
+        </div>
+
+      </Card>
     </div>
   );
 };
